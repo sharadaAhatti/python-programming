40694f32b408356d0c2e8ee9a062988ff81c7202
sharadaAhatti <110833178+sharadaAhatti@users.noreply.github.com>
2023-08-06 13:23:09 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,5 @@
+celsius_temp = int(input('enter celsisus temp:'))
+fahr_temp = (celsius_temp * 9/5) + 32
+print(f'{celsius_temp} degree of celsius is equal to {fahr_temp} degree fahrenheit')
+#or
+print('celsius_temp:',(fahr_temp))
