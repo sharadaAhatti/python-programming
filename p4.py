5e4d4f7e440bebb711144c93a53a1c7100b57c20
sharadaAhatti <110833178+sharadaAhatti@users.noreply.github.com>
2023-08-06 13:03:34 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,12 @@
+num1 = int(input('enter a first number:'))
+num2 = int(input('enter a second number:'))
+#before swapping
+print('Numbers before swapping')
+print('first number:',num1)
+print('second number:',num2)
+#logic for swapping
+num1, num2 = num2, num1
+#after swapping
+print('Numbers after swapping')
+print('first number:',num1)
+print('second number:',num2)
